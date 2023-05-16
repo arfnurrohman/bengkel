7153914982689fe3a6365a8786adb6c9c95fecb8
arfnurrohman <201220032@unmuhpnk.ac.id>
2023-05-16 20:00:21 +0700

    halaman 1

---
+++ app/Http/Controllers/HalamanController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HalamanController extends Controller
+{
+  public function halaman1()
+  {
+  return view("halaman/halaman1");
+}
+public function halaman2()
+  {
+  return view("halaman/halaman2");
+}
+public function halaman3()
+  {
+  return view("halaman/halaman3");
+}
+}
---
+++ resources/views/halaman/halaman3.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>halaman3</h1>
+@endsection
