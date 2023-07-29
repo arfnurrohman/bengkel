67989e2310a00da4c8ff844ec748a931354a10e8
Rizki Kurnia <rizkikurniapinoh@gmail.com>
2023-07-30 02:48:22 +0700

    add google map halaman depan

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -380,11 +380,53 @@
                 </form>
             </div>
         </section>
+        <head>
+
+
+    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+    <style type="text/css">
+        #map {
+          height: 400px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container mt-5">
+      <center>  <h2>Lokasi Toko Sepeda !!!!!</h2></center>
+        <div id="map"></div>
+    </div>
+
+    <script type="text/javascript">
+        function initMap() {
+          const myLatLng = { lat: -0.109612, lng: 109.406040 };
+          const map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 5,
+            center: myLatLng,
+          });
+  
+          new google.maps.Marker({
+            position: myLatLng,
+            map,
+            title: "Hello Rajkot!",
+          });
+        }
+  
+        window.initMap = initMap;
+    </script>
+
+    <script type="text/javascript"
+        src="https://maps.google.com/maps/api/js?key={{ env('GOOGLE_MAP_KEY') }}&callback=initMap" ></script>
+
+</body>
+<p></p>
+  <hr class="featurette-divider">
+
         <!-- Footer-->
         <footer class="footer py-4">
             <div class="container">
                 <div class="row align-items-center">
-                    <div class="col-lg-4 text-lg-start">Copyright &copy; Your Website 2023</div>
+                    <div class="col-lg-4 text-lg-start">© 2017–2023 Company, Inc.</div>
                     <div class="col-lg-4 my-3 my-lg-0">
                         <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Twitter"><i class="fab fa-twitter"></i></a>
                         <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Facebook"><i class="fab fa-facebook-f"></i></a>
@@ -608,6 +650,7 @@
                 </div>
             </div>
         </div>
+        
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
