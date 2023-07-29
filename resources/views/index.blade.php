c8d826026219ba6daae551c4cc21493024e8e2c8
Rizki Kurnia <rizkikurniapinoh@gmail.com>
2023-07-30 03:45:10 +0700

    add profil & deskripsi team

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -380,48 +380,6 @@
                 </form>
             </div>
         </section>
-        <head>
-
-
-    <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
-    <style type="text/css">
-        #map {
-          height: 400px;
-        }
-    </style>
-</head>
-
-<body>
-    <div class="container mt-5">
-      <center>  <h2>Lokasi Toko Sepeda !!!!!</h2></center>
-        <div id="map"></div>
-    </div>
-
-    <script type="text/javascript">
-        function initMap() {
-          const myLatLng = { lat: -0.109612, lng: 109.406040 };
-          const map = new google.maps.Map(document.getElementById("map"), {
-            zoom: 5,
-            center: myLatLng,
-          });
-  
-          new google.maps.Marker({
-            position: myLatLng,
-            map,
-            title: "Hello Rajkot!",
-          });
-        }
-  
-        window.initMap = initMap;
-    </script>
-
-    <script type="text/javascript"
-        src="https://maps.google.com/maps/api/js?key={{ env('GOOGLE_MAP_KEY') }}&callback=initMap" ></script>
-
-</body>
-<p></p>
-  <hr class="featurette-divider">
-
         <!-- Footer-->
         <footer class="footer py-4">
             <div class="container">
@@ -650,7 +608,6 @@
                 </div>
             </div>
         </div>
-        
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
